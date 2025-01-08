af62d10e5dcdb23e1e912a78704495da1c55df1e
中島　亮三 <47051364+a-nakajima-at-shokuryu@users.noreply.github.com>
2025-01-08 14:27:15 +0900

    Create pgui.py

---
+++ utilities/pgui.py
@@ -0,0 +1,14 @@
+from utilities.common import * 
+import pyautogui as pgui 
+import pyperclip as clip 
+
+def pgui_write(s):
+  clip.copy(s) 
+  pgui.hotkey("ctrl", "v")
+
+## test code: 
+# pgui.hotkey("win")
+# time.sleep(1)
+# pgui_write("リモートデスクトップ接続")
+# time.sleep(1)
+# pgui.hotkey("enter")
